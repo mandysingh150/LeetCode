7fb286c100e4476d625e0c33d71b67cb3b1e332e
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-15 13:17:54 +0530

    Time: 4 ms (32.86%), Space: 5.8 MB (77.72%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,14 +1,8 @@
-#define mod ((int)(1e9+7))
 class Solution {
 public:
-    int h(int n) {
-        if(n<=0)
-            return 0;
-        if(n==1)
-            return 1;
-        return (1ll*n*(2*n-1)*h(n-1))%mod;
-    }
     int countOrders(int n) {
-        return h(n)%mod;
+        if(n<=0) return 0;
+        if(n==1) return 1;
+        return (1ll*n*(2*n-1)*countOrders(n-1))%((int)(1e9+7));
     }
 };
