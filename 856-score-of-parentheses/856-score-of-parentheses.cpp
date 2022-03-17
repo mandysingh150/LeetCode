75ec02137a33ac50549533e2b27a0465e7a2292f
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-17 12:37:20 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    int get_score(string &s, vector<int> &ei, int in) {
-        if(in > s.size())
-            return 0;
-        if(ei[in] == in+1)
-            return 1;
-        
-        int val=0;
-        for(int i=in+1 ; i<ei[in] ; ++i) {
-            val += get_score(s, ei, i);
-            i=ei[i];
-        }
-        return val<<1;
-    }
-    int scoreOfParentheses(string s) {
-        vector<int> ei(s.size(), -1);
-        stack<int> st;
-        for(int i=0 ; i<s.size() ; ++i) {
-            if(s[i]=='(') 
-                st.push(i);
-            else {
-                ei[st.top()] = i;
-                st.pop();
+    int scoreOfParentheses(string S) {
+        stack<int> stack;
+        int cur = 0;
+        for (char i : S)
+            if (i == '(') {
+                stack.push(cur);
+                cur = 0;
             }
-        }
-        if(ei[0] != s.size()-1) {
-            string t = "(" + s + ")";
-            vector<int> temp(t.size(), -1);
-            temp[0]=t.size()-1;
-            for(int i=1 ; i<t.size()-1 ; ++i)
-                temp[i] = ei[i-1]==-1 ? ei[i-1] : ei[i-1]+1;
-            return get_score(t, temp, 0)>>1;
-        }
-        return get_score(s, ei, 0);
+            else {
+                cur = stack.top() + max(cur*2, 1);
+                stack.pop();
+            }
+        return cur;
     }
 };
