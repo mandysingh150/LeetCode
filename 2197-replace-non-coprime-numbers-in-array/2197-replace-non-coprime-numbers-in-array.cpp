de0ca97348e3d70e7ab779a3b2494830b3340c6f
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-17 21:41:09 +0530

    Time: 395 ms (24.13%), Space: 120.9 MB (78.90%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-        if (a == 0)
-            return b;
-        return gcd(b % a, a);
-    }
     vector<int> replaceNonCoprimes(vector<int>& a) {
-        stack<int> st;
-        st.push(a[0]);
-        for(int i=1 ; i<a.size() ; ++i) {
-            st.push(a[i]);
-            while(st.size()>=2) {
-                int n1=st.top(); st.pop();
-                int n2=st.top(); st.pop();
-                int my_gcd=gcd(n1, n2);
-                if(my_gcd > 1)
-                    st.push(1ll*n1*n2/my_gcd);
-                else {
-                    st.push(n2);
-                    st.push(n1);
-                    break;
-                }
+        vector<int> v;
+        for(int i=0 ; i<a.size() ; ++i) {
+            v.emplace_back(a[i]);
+            while(v.size()>1 and __gcd(v.back(), v[v.size()-2])>1) {
+                long long a=v.back(), b=v[v.size()-2], gcd=__gcd(a,b);
+                v.pop_back();
+                v.pop_back();
+                v.push_back(a*b/gcd);
             }
         }
-        a.clear();
-        while(!st.empty()) {
-            a.push_back(st.top());
-            st.pop();
-        }
-        reverse(begin(a), end(a));
-        return a;
+        return v;
     }
 };
