35b9806292e83412548357680bc57eb9576fa4a5
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-20 00:25:48 +0530

    Time: 7 ms (92.28%), Space: 7.3 MB (84.37%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -5,18 +5,12 @@ public:
         // add = i, j-1
         // remove = i-1, j
         int dp[word1.size()+1][word2.size()+1];
-        for(int i=0 ; i<=word1.size() ; ++i) {
-            for(int j=0 ; j<=word2.size() ; ++j) {
+        for(int i=0 ; i<=word1.size() ; ++i)
+            for(int j=0 ; j<=word2.size() ; ++j)
                 if(i==0 or j==0)
                     dp[i][j] = i==0 ? j : i;
-                else {
-                    if(word1[i-1] == word2[j-1])
-                        dp[i][j] = dp[i-1][j-1];
-                    else
-                        dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
-                }
-            }
-        }
+                else
+                    dp[i][j] = word1[i-1] == word2[j-1] ? dp[i-1][j-1] : 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
         return dp[word1.size()][word2.size()];
     }
 };
