7dc74efdb5cbd019917f7c56a5c3158ee3bb68ca
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-19 15:10:10 +0530

    Time: 13 ms (96.40%), Space: 12.4 MB (74.50%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& a) {
+        int n=a.size(), ending_mask=(1<<n)-1;
+        vector<vector<int>> vis(n, vector<int>(ending_mask+1, 0));
+        // {current_node, mask}
+        queue<pair<int,int>> q;
+        for(int i=0 ; i<n ; ++i) {
+            q.push({i, (1<<i)});
+            vis[i][(1<<i)]=1;
+        }
+        int len = 0;
+        while(!q.empty()) {
+            int sz=q.size();
+            while(sz--) {
+                auto [node, mask] = q.front(); 
+                q.pop();
+                if(mask == ending_mask)
+                    return len;
+                for(auto i: a[node]) {
+                    int new_mask = (1<<i)|mask;
+                    if(vis[i][new_mask] == 0) {
+                        q.push({i, new_mask});
+                        vis[i][new_mask]=1;
+                    }                        
+                }
+            }
+            len++;
+        }
+        return -1;
+    }
+};
