746adb0577cc17cd6d6408bc9519e3ac34217d87
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-22 14:44:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (32.59%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int reverse(int x) {
-        if(x==INT_MAX or x==INT_MIN) 
+        int sign = x<0 ? -1 : 1;
+        string s = to_string(1LL*sign*x);
+        std::reverse(begin(s), end(s));
+        if(stoll(s)*sign>INT_MAX or stoll(s)*sign<INT_MIN)
             return 0;
-        bool is_neg=0;
-        if(x<0) {
-            is_neg=1;
-            x*=-1;
-        }
-        long long s=0;
-        while(x>0) {
-            s = 10*s + x%10;
-            x/=10;
-        }
-        if(is_neg) 
-            s*=-1;
-        if(s>INT_MAX or s<INT_MIN) 
-            return 0;
-        return s;
+        return sign*stoll(s);
     }
 };
