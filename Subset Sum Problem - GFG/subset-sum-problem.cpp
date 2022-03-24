6e0f03647f153acd20785fc19be33134e2524cdf
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-24 19:35:35 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -14,15 +14,13 @@ public:
         bool dp[N+1][sum+1];
         for(int i=0 ; i<N+1 ; ++i) {
             for(int j=0 ; j<sum+1 ; ++j) {
-                if(i==0)
-                    dp[i][j] = 0;
-                if(j==0)
-                    dp[i][j] = 1;
-            }
-        }
-        for(int i=1 ; i<N+1 ; ++i) {
-            for(int j=1 ; j<sum+1 ; ++j) {
-                if(arr[i-1] <= j) {
+                if(i==0 or j==0) {
+                    if(i==0)
+                        dp[i][j] = 0;
+                    if(j==0)
+                        dp[i][j] = 1;
+                }
+                else if(arr[i-1] <= j) {
                     dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j];
                 }
                 else {
