420bf098aa86d2c7c9e6634f45707b9f1587679e
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-03-28 10:35:52 +0530

    Time: 9 ms (46.39%), Space: 13.9 MB (71.85%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     bool search(vector<int>& a, int target) {
-        for(int i=0 ; i<a.size() ; ++i)
-            if(a[i] == target)
+        int l=0, h=a.size()-1, mid;
+        while(l<h) {
+            mid=(l+h)>>1;
+            if(a[mid] == target)
                 return 1;
-        return 0;
-                
-        // int l=0,h=a.size()-1;
-        // while(l<h) {
-        //     int mid=(l+h)>>1;
-        //     if(a[mid]<a[h])
-        //         h=mid;
-        //     else if(a[mid] > a[h])
-        //         l=mid+1;
-        //     else 
-        //         h--;
-        // }
-        // int k=l;    // 'l' is equal to 'h'
-        // l=0, h=a.size()-1;
-        // while(l<=h) {
-        //     int mid=(l+h)>>1;
-        //     int realMid=(mid+k)%a.size();
-        //     if(a[realMid] == target)
-        //         return 1;
-        //     else if(a[realMid] < target)
-        //         l=mid+1;
-        //     else
-        //         h=mid-1;
-        // }
-        // return 0;
+            if(a[mid] < a[h]) {
+                if(a[mid]<target and target<=a[h])
+                    l=mid+1;
+                else
+                    h=mid;
+            }
+            else if(a[mid] > a[h]) {
+                if(a[l]<=target and target<a[mid])
+                    h=mid;
+                else
+                    l=mid+1;
+            }
+            else
+                h--;    // to decrease the search space
+        }
+        return a[l]==target;
     }
 };
