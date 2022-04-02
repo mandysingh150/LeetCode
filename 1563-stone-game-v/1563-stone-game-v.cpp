29e1ed55b9365ab05c812c654fd8eeac2b51b48f
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-04-02 18:39:07 +0530

    Time: 762 ms (42.99%), Space: 10.9 MB (60.39%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    int dp[502][502];
     int help(vector<int> &a, int si, int ei) {
         if(si >= ei)
             return 0;
@@ -11,20 +11,17 @@ public:
         int val=0;
         for(int i=si ; i<ei ; ++i) {
             int sum1 = a[i]-a[si-1], sum2 = a[ei]-a[i];
-            if(sum1 == sum2)
-                val = max(val, max(sum1 + help(a, si, i), sum2 + help(a, i+1, ei)));
-            else if(sum1 > sum2)
+            if(sum1 >= sum2)
                 val = max(val, sum2 + help(a, i+1, ei));
-            else
+            if(sum2 >= sum1)
                 val = max(val, sum1 + help(a, si, i));
         }
         return dp[si][ei] = val;
     }
     int stoneGameV(vector<int>& a) {
-        int n=a.size();
-        dp.resize(n+1, vector<int>(n+1, -1));
+        memset(dp, -1, sizeof(dp));
         vector<int> pre={0};
         for(auto i: a) pre.push_back(pre.back() + i);
-        return help(pre, 1, n);
+        return help(pre, 1, a.size());
     }
 };
