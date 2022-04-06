6d648c2dcfcae727663189bfcb11e1853d75cea8
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-04-06 18:02:41 +0530

    Time: 12 ms (68.59%), Space: 13.5 MB (9.34%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
@@ -1,34 +1,38 @@
-int dir[]={-1,0,1,0,-1};
+int dir[] = {-1,0,1,0,-1};
 class Solution {
 public:
-    int cnt, m, n;
-    void dfs(vector<vector<int>> &v, int i, int j) {
-        v[i][j]=1;
-        for(int k=0 ; k<4 ; ++k) {
-            int x=i+dir[k], y=j+dir[k+1];
-            if(x>=0 and x<v.size() and y>=0 and y<v[0].size() and v[x][y]==0)
-                dfs(v, x, y);
-        }
-    }
     int regionsBySlashes(vector<string>& a) {
-        m=a.size(), n=a[0].size(), cnt=0;
-        vector<vector<int>> v(m*3, vector<int>(n*3, 0));
-        for(int i=0 ; i<m ; ++i) {
+        int n=a.size();
+        int dp[n*3][n*3];
+        memset(dp, 0, sizeof(dp));
+        
+        for(int i=0 ; i<n ; ++i) {
             for(int j=0 ; j<n ; ++j) {
-                if(a[i][j] == '/') {
-                    v[i*3][j*3+2] = v[i*3+1][j*3+1] = v[i*3+2][j*3] = 1;
-                }
-                else if(a[i][j] == '\\') {
-                    v[i*3][j*3] = v[i*3+1][j*3+1] = v[i*3+2][j*3+2] = 1;
-                }
+                if(a[i][j] == '/')
+                    dp[i*3][j*3+2] = dp[i*3+1][j*3+1] = dp[i*3+2][j*3] = 1;
+                else if(a[i][j] == '\\')
+                    dp[i*3][j*3] = dp[i*3+1][j*3+1] = dp[i*3+2][j*3+2] = 1;
             }
         }
-        for(int i=0 ; i<m*3 ; ++i) {
-            for(int j=0 ; j<n*3 ; ++j) {
-                if(v[i][j] == 0) {
+        int cnt=0;
+        for(int i=0 ; i<3*n ; ++i) {
+            for(int j=0 ; j<3*n ; ++j) {
+                if(dp[i][j]==0) {
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    dp[i][j] = 1;
                     cnt++;
-                    dfs(v, i, j);
-                }                    
+                    while(!q.empty()) {
+                        auto [row, col] = q.front();
+                        q.pop();
+
+                        for(int k=0 ; k<4 ; ++k) {
+                            int x = row+dir[k], y = col+dir[k+1];
+                            if(x>=0 and x<3*n and y>=0 and y<3*n and dp[x][y]==0)
+                                q.push({x, y}), dp[x][y]=1;
+                        }
+                    }
+                }
             }
         }
         return cnt;
