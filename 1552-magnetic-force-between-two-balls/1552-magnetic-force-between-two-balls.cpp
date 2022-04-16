49c1fabdd7b9bd99f3e469c00b6eadc4868db809
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-04-16 17:15:27 +0530

    Time: 332 ms (25.58%), Space: 57.8 MB (81.49%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool help(vector<int> &v, int skip, int tot) {
-        tot--;  // one ball is placed on v[i]
-        int i=0;
-        vector<int>::iterator it;
-        while(tot and (it=lower_bound(v.begin()+i, v.end(), v[i]+skip))!=v.end()) {
-            i = it-v.begin();
-            tot--;
+        int pos = v[0];
+        for(int i=1 ; i<v.size() ; ++i) {
+            if(v[i] - pos >= skip) {
+                --tot;
+                pos = v[i];
+            }
         }
-        return tot==0;
+        return tot<=1;
     }
     int maxDistance(vector<int>& a, int m) {
         sort(a.begin(), a.end());
         int l=1, h=a.back()-l, ans=1;
         while(l<=h) {
             int mid = (l+h)/2;
-            if(help(a, mid, m)) {
-                ans = mid;
-                l = mid+1;
-            }
+            if(help(a, mid, m))
+                ans = mid, l = mid+1;
             else
                 h = mid-1;
         }
