7a0982f001973cbe871c7047b68ef27a413a8bb9
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-04-18 23:09:54 +0530

    Time: 103 ms (92.22%), Space: 61.3 MB (61.69%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    int findDuplicate(vector<int> a) {
-        int n = a.size();
-        
-        // Approach 1 - Floyd's Tortiose and Hare algorithm
-        int slow = 0, fast = 0;
-        for(int i=0 ; i<n ; ++i) {
-            slow = a[slow];
-            fast = a[a[fast]];
-            
-            if(fast==slow) {
-                slow = 0;
-                while(slow!=fast) {
-                    slow = a[slow];
-                    fast = a[fast];
-                }
-                return slow;    // or return fast as (slow==fast)
-            }
+    int findDuplicate(vector<int>& nums) {
+        for(int i=0 ; i<nums.size() ; ++i) {
+            if(nums[abs(nums[i])] < 0)
+                return abs(nums[i]);
+            nums[abs(nums[i])] *= -1;
         }
         return -1;
     }
