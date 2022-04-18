f7b70c3ae224ca6013f2c49600b45df25efb3df4
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-04-18 10:44:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.06%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,19 +1,15 @@
 #define ll long long
 class Solution {
 public:
-    long long find_sum(long long n) {
-        return (n*(n+1))>>1;
-    }
     int maxValue(int n, int index, int maxSum) {
+        auto find_sum = [](long long n) { return n*(n+1)>>1; };
+        
         ll l=1, h=maxSum, ans;
         while(l<=h) {
             ll mid = (l+h)>>1;
             
             // processing
-            ll sum = mid;
-            ll left = find_sum(mid-1) + (mid > index ? -find_sum(mid-index-1) : index-mid+1);
-            ll right = find_sum(mid-1) + (mid >= n-index ? -find_sum(mid-(n-index)) : n-index-mid);
-            sum += left+right;
+            ll sum = mid + (find_sum(mid-1) + (mid > index ? -find_sum(mid-index-1) : index-mid+1)) + (find_sum(mid-1) + (mid >= n-index ? -find_sum(mid-(n-index)) : n-index-mid));
             
             if(sum <= maxSum) {
                 ans = mid;
