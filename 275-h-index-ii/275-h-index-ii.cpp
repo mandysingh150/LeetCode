61c5215c78bbe8d08be597de32ba325136771ced
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-04-19 00:33:42 +0530

    Time: 11 ms (97.96%), Space: 18.5 MB (89.27%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -4,7 +4,6 @@ public:
         int l=0, h=a.size()-1, ans=0;
         while(l<=h) {
             int mid=(l+h)>>1;
-            // int in = lower_bound(begin(a), end(a), amid)-begin(a);
             if(a.size()-mid <= a[mid])
                 ans=a.size()-mid, h=mid-1;
             else
