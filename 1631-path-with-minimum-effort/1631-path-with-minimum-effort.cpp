f8d9380dde35710f44ebc8551a0a0f886ba8598f
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-04-28 05:55:17 +0530

    Time: 630 ms (22.01%), Space: 55.2 MB (20.30%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,34 +1,26 @@
 int dir[] = {-1,0,1,0,-1};
-
 class Solution {
 public:
-    bool isPossible(vector<vector<int>>& h, int val) {
-        queue<pair<int,int>> q;
-        vector<vector<int>> v(h.size(), vector<int>(h[0].size(), 0));
-        q.push({0,0});
-        v[0][0] = 1;
-        while(!q.empty()) {
-            auto [row, col] = q.front();
-            q.pop();
-            
-            if(row==h.size()-1 and col==h[0].size()-1)
-                return 1;
-            
-            for(int k=0 ; k<4 ; ++k) {
-                int x = dir[k]+row, y = dir[k+1]+col;
-                if(x>=0 and x<h.size() and y>=0 and y<h[0].size() and abs(h[x][y]-h[row][col])<=val and !v[x][y]) {
-                    v[x][y] = 1;
-                    q.push({x,y});
-                }
+    vector<vector<int>> v;
+    void isPossible(vector<vector<int>>& h, int row, int col, int val) {
+        for(int k=0 ; k<4 ; ++k) {
+            int x = dir[k]+row, y = dir[k+1]+col;
+            if(x>=0 and x<h.size() and y>=0 and y<h[0].size() and abs(h[x][y]-h[row][col])<=val and !v[x][y]) {
+                v[x][y] = 1;
+                isPossible(h, x, y, val);
             }
         }
-        return 0;
     }
     int minimumEffortPath(vector<vector<int>>& he) {
         int l=0, h=INT_MAX, ans;
         while(l<=h) {
             int mid = (l+h)>>1;
-            if(isPossible(he, mid)) {
+            
+            v = vector<vector<int>>(he.size(), vector<int>(he[0].size(), 0));
+            v[0][0] = 1;
+            isPossible(he, 0, 0, mid);
+            
+            if(v[he.size()-1][he[0].size()-1]) {
                 h=mid-1;
                 ans = mid;
             }
