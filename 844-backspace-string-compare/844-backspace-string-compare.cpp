13f7c7ac3a55ecfbbfd31136f58f2dfeda534d96
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-05-01 07:08:19 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (39.15%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -3,29 +3,21 @@ public:
     bool backspaceCompare(string s, string t) {
         int in1=0, in2=0;
         for(int i=0 ; i<s.size() ; ++i) {
-            if(s[i] == '#') {
-                if(in1 > 0)
-                    in1--;
-            }                
-            else {
-                s[in1] = s[i];
-                in1++;
+            if(s[i] == '#') { 
+                if(in1 > 0) in1--; 
             }
+            else
+                s[in1] = s[i], in1++;
         }
         for(int i=0 ; i<t.size() ; ++i) {
             if(t[i] == '#') {
-                if(in2 > 0)
-                    in2--;
+                if(in2 > 0) in2--;
             }
-            else {
-                t[in2] = t[i];
-                in2++;
-            }
+            else
+                t[in2] = t[i], in2++;
         }
         if(in1 != in2) return 0;
-        for(int i=0 ; i<in1 ; ++i)
-            if(s[i] != t[i])
-                return 0;
+        for(int i=0 ; i<in1 ; ++i) if(s[i] != t[i]) return 0;
         return 1;
     }
 };
