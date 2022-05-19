4c2707124734cde689fd334df4c3704d7f8ae9a2
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-05-20 01:20:05 +0530

    Time: 394 ms (77.57%), Space: 115.2 MB (55.23%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -5,9 +5,8 @@ public:
         int ans = INT_MAX;
         for(int i=0 ; i<a.size() ; ++i) {
             if(mp.count(a[i]))
-                if(i-mp[a[i]]+1 < ans) {
+                if(i-mp[a[i]]+1 < ans)
                     ans = i-mp[a[i]]+1;
-                }
             mp[a[i]] = i;
         }
         return ans == INT_MAX ? -1 : ans;
