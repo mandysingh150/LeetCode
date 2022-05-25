fe8b9263a011a20896860f08176ab2b100d33a09
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-05-26 01:26:30 +0530

    Time: 519 ms (43.54%), Space: 77.6 MB (70.81%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& a) {
+        // sorting in ASCENDING order of widths and DESCENDING order of heights
         sort(begin(a), end(a), [](vector<int> &c, vector<int> &d) {
             if(c[0] == d[0])
                 return c[1] > d[1];
             return c[0] < d[0];
         });
         
-        vector<int> LIS;
-        LIS.push_back(a[0][1]);
+        vector<int> lis;
+        lis.push_back(a[0][1]);
         for(int i=0 ; i<a.size() ; ++i) {
-            auto index = lower_bound(begin(LIS), end(LIS), a[i][1]) - begin(LIS);
-            if(index >= LIS.size())
-                LIS.push_back(a[i][1]);
+            auto index = lower_bound(begin(lis), end(lis), a[i][1]) - begin(lis);
+            if(index >= lis.size())
+                lis.push_back(a[i][1]);
             else
-                LIS[index] = a[i][1];
+                lis[index] = a[i][1];
         }        
-        return LIS.size();
+        return lis.size();
     }
 };
