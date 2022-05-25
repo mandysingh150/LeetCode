cfe997998106238d0461a1dd10ab99c4aa781f9f
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-05-26 01:33:12 +0530

    Time: 822 ms (8.46%), Space: 77.6 MB (44.26%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& a) {
         // sorting in ASCENDING order of widths and DESCENDING order of heights
+        // Now why we need to sort in decreasing order, if two widths are same. By this practice, we're assuring that no width will get counted more than once
         sort(begin(a), end(a), [](vector<int> &c, vector<int> &d) {
             if(c[0] == d[0])
                 return c[1] > d[1];
             return c[0] < d[0];
         });
         
+        // after sorting, we have to only apply LIS on heights field
+        // how reverse sorting the height would remove duplicacy? As the name itself says, Longest Increasing Subsequnce, the next coming height would be less than the previous one. Hence, forbidding it to increase length count.
         vector<int> lis;
         lis.push_back(a[0][1]);
         for(int i=0 ; i<a.size() ; ++i) {
