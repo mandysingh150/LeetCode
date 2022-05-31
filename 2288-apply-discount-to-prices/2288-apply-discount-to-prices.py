4e4218b9ba4da41b19d0ca9663673cc72beec17e
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-05-31 18:46:11 +0530

    Time: 221 ms (53.04%), Space: 16 MB (69.01%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.py
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.py
@@ -1,10 +1,10 @@
 class Solution:
     def discountPrices(self, sentence: str, discount: int) -> str:
         ans = []
-        for word in sentence.split():
+        words = sentence.split()
+        for i, word in enumerate(words):
             if word[0]=='$' and word[1:].isnumeric():
-                price = int(word[1:])
-                ans.append('${:.2f}'.format(price-(price*discount/100.0)))
-            else:
-                ans.append(word)
-        return ' '.join(ans)
+                words[i] = f'${(((100-discount)/100.0)*int(word[1:])):.2f}'
+        return ' '.join(words)
+    
+    
