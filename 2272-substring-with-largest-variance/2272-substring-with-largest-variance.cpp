994960e2c044058a8ad33e71bbfd79be525640ec
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-03 17:17:24 +0530

    Time: 216 ms (63.99%), Space: 6.7 MB (96.15%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -12,19 +12,22 @@ public:
                     int i_count=0, j_count=0;
                     bool can_use_previously_occured_j=0;
                     for(auto ch: s) {
-                        if(ch == i) i_count++;
-                        if(ch == j) j_count++;
+                        if(ch == i) 
+                            i_count++;
+                        if(ch == j) 
+                            j_count++;
                         
                         if(j_count > 0) {
                             ans = max(ans, i_count-j_count);
                         }
+                        // edge case: use previously occured 'j'
                         else if(j_count==0 and can_use_previously_occured_j) {
                             // using previously occured 'j', so subtracting "1" from 'i_count'
                             ans = max(ans, i_count - 1);
                         }
                         
                         if(j_count > i_count) {
-                            // reset the counts of i and j
+                            // reset the counts of 'i' and 'j'
                             i_count = j_count = 0;
                             can_use_previously_occured_j = 1;
                         }
