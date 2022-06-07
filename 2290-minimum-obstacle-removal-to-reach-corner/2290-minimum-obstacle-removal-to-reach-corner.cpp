d367fefcf61328376bb8e30b987886269a3575bc
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-07 17:01:51 +0530

    Time: 1390 ms (31.43%), Space: 104 MB (85.39%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,27 +1,27 @@
 int dir[]={-1,0,1,0,-1};
-#define piii pair<int,pair<int,int>>
+#define tup tuple<int,int,int>
 class Solution {
 public:
-    // Dijkstra's algorithm for single-source shortest path problem
     int minimumObstacles(vector<vector<int>>& a) {
+        // Dijkstra's algorithm
         int m=a.size(), n=a[0].size();
-        priority_queue<piii, vector<piii>, greater<piii>> pq;
+        // {obstacles, row, col}
+        priority_queue<tup, vector<tup>, greater<tup>> pq;
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX-1));
         dist[0][0] = a[0][0];
-        pq.push({0, {0, 0}});
+        pq.push({0,0,0});
         while(!pq.empty()) {
-            auto top = pq.top();
+            auto [val, r, c] = pq.top();
             pq.pop();
             
-            int val=top.first, r=top.second.first, c=top.second.second;
             if(r==m-1 and c==n-1)
                 return val;
             
             for(int i=0 ; i<4 ; ++i) {
-                int row=r+dir[i] ,col=c+dir[i+1];
-                if(row>=0 and row<m and col>=0 and col<n and val + a[row][col] < dist[row][col]) {
-                    dist[row][col] = val + a[row][col];
-                    pq.push({dist[row][col], {row, col}});
+                int row=r+dir[i], col=c+dir[i+1];
+                if(row>=0 and row<m and col>=0 and col<n and a[row][col]+val<dist[row][col]) {
+                    dist[row][col] = a[row][col]+dist[r][c];
+                    pq.push(tup{dist[row][col], row, col});
                 }
             }
         }
