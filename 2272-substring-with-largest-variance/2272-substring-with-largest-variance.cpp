7d51ec6bb9b3962ddbd8d1ca255dae871c0b1697
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-10 01:28:10 +0530

    Time: 359 ms (39.18%), Space: 6.6 MB (100.00%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     int largestVariance(string &s) {
-        // understood from post by "dm_a"
-        
-        int ans=0;
-        for(char i='a' ; i<='z' ; ++i) {
-            for(char j='a' ; j<='z' ; ++j) {
-                if(i != j) {
-                    // 'i' is the high frequency character
-                    // 'j' is the low frequency character
-                    int i_count=0, j_count=0;
-                    bool can_use_previously_occured_j=0;
-                    for(auto ch: s) {
-                        if(ch == i) 
-                            i_count++;
-                        if(ch == j) 
-                            j_count++;
-                        
-                        if(j_count > 0) {
-                            ans = max(ans, i_count-j_count);
-                        }
-                        // edge case: use previously occured 'j'
-                        else if(j_count==0 and can_use_previously_occured_j) {
-                            // using previously occured 'j', so subtracting "1" from 'i_count'
-                            ans = max(ans, i_count - 1);
-                        }
-                        
-                        if(j_count > i_count) {
-                            // reset the counts of 'i' and 'j'
-                            i_count = j_count = 0;
-                            can_use_previously_occured_j = 1;
-                        }
+        int mx=0;
+        // assuming 'ch1' has higher frequency than 'ch2'
+        for(char ch1='a' ; ch1<='z' ; ++ch1) {
+            for(char ch2='a'; ch2<='z' ; ++ch2) {
+                if(ch1 == ch2)
+                    continue;
+                
+                int c1=0, c2=0;
+                bool can_use_prev_c2=0;
+                for(int i=0 ; i<s.size() ; ++i) {
+                    if(s[i] == ch1) c1++;
+                    if(s[i] == ch2) c2++;
+                    
+                    if(c2>0) {
+                        mx = max(mx, c1-c2);
+                    }
+                    else if(c2==0 and can_use_prev_c2) {
+                        mx = max(mx, c1-1);
+                    }
+                    
+                    if(c1 < c2) {
+                        c1=c2=0;
+                        can_use_prev_c2 = 1;
                     }
                 }
             }
         }
-        return ans;
+        return mx;
     }
 };
