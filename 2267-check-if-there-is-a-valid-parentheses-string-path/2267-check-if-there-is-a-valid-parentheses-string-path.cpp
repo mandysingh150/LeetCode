058387f4c59d1439b547728a5cdef9e524aab4aa
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-10 04:41:10 +0530

    Time: 1256 ms (11.83%), Space: 138.4 MB (24.33%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -1,31 +1,33 @@
+#define tup tuple<int,int,int>
 class Solution {
 public:
     bool hasValidPath(vector<vector<char>>& a) {
         if(a[0][0] == ')')
             return 0;
         
+        // {left_cnt-right_cnt, row, col}
+        queue<tup> q;
         int m=a.size(), n=a[0].size();
-        vector<pair<int,int>> d = {{0,1}, {1,0}};
-        queue<tuple<int,int,int>> q;
+        int dir[2][2] = {{1,0}, {0,1}};
         int vis[1001][100][100];
         memset(vis, 0, sizeof(vis));
         
-        q.push({1, 0, 0});
+        q.push({1,0,0});
         vis[1][0][0] = 1;
         while(!q.empty()) {
-            auto [diff, r, c] = q.front();
+            auto [diff, row, col] = q.front();
             q.pop();
             
-            if(r==m-1 and c==n-1 and diff==0)
+            if(row==m-1 and col==n-1 and diff==0)
                 return 1;
             
             for(int i=0 ; i<2 ; ++i) {
-                int row=r+d[i].first, col=c+d[i].second;
-                if(row<m and col<n) {
-                    int new_diff = diff + (a[row][col]=='(' ? 1 : -1);
-                    if(new_diff>=0 and new_diff<=(m+n)/2 and vis[new_diff][row][col]==0) {
-                        q.push({new_diff, row, col});
-                        vis[new_diff][row][col] = 1;
+                int r=row+dir[i][0], c=col+dir[i][1];
+                if(r<m and c<n) {
+                    int new_diff = diff + (a[r][c]=='(' ? 1 : -1);
+                    if(new_diff>=0 and new_diff<=(m+n)/2 and vis[new_diff][r][c] == 0) {
+                        q.push({new_diff, r, c});
+                        vis[new_diff][r][c] = 1;
                     }
                 }
             }
