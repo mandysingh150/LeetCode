a354431837bd5fb841d6ba221fc812b294010d0d
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-17 02:24:29 +0530

    Time: 1463 ms (8.35%), Space: 138.5 MB (24.14%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -2,32 +2,32 @@
 class Solution {
 public:
     bool hasValidPath(vector<vector<char>>& a) {
-        if(a[0][0] == ')')
+        if(a[0][0] == ')') {
             return 0;
-        
-        // {left_cnt-right_cnt, row, col}
-        queue<tup> q;
+        }
         int m=a.size(), n=a[0].size();
-        int dir[2][2] = {{1,0}, {0,1}};
+        queue<tup> q;
         int vis[1001][100][100];
+        int dir[2][2] = {{1,0}, {0,1}};
         memset(vis, 0, sizeof(vis));
         
         q.push({1,0,0});
         vis[1][0][0] = 1;
         while(!q.empty()) {
-            auto [diff, row, col] = q.front();
+            // auto [val, r, c] = q.top();
+            auto [val, r, c] = q.front();
             q.pop();
             
-            if(row==m-1 and col==n-1 and diff==0)
+            if(r==m-1 and c==n-1 and val==0)
                 return 1;
             
             for(int i=0 ; i<2 ; ++i) {
-                int r=row+dir[i][0], c=col+dir[i][1];
-                if(r<m and c<n) {
-                    int new_diff = diff + (a[r][c]=='(' ? 1 : -1);
-                    if(new_diff>=0 and new_diff<=(m+n)/2 and vis[new_diff][r][c] == 0) {
-                        q.push({new_diff, r, c});
-                        vis[new_diff][r][c] = 1;
+                int x=dir[i][0]+r, y=dir[i][1]+c;
+                if(x>=0 and y>=0 and x<m and y<n) {
+                    int new_val = val + (a[x][y]=='(' ? 1 : -1);
+                    if(new_val>=0 and new_val<=(m+n)/2 and !vis[new_val][x][y]) {
+                        vis[new_val][x][y] = 1;
+                        q.push(tup{new_val, x, y});
                     }
                 }
             }
