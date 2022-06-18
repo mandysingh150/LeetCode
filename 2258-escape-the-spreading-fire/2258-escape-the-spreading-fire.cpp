6178533ebd72783279f14992df366f1aa96fad46
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-18 17:45:12 +0530

    Time: 124 ms (65.32%), Space: 22 MB (77.42%) - LeetHub

--- 2258-escape-the-spreading-fire/2258-escape-the-spreading-fire.cpp
+++ 2258-escape-the-spreading-fire/2258-escape-the-spreading-fire.cpp
@@ -2,46 +2,45 @@
 class Solution {
 public:
     int maximumMinutes(vector<vector<int>>& a) {
-        auto find_corner_three = [&](bool is_fire) -> vector<int> {
-            vector<vector<int>> v(a.size(), vector<int>(a[0].size(), INT_MAX));
-            queue<pair<int,int>> q;
-            int m=a.size(), n=a[0].size();
+        int m=a.size(), n=a[0].size();
+        auto bfs = [&](bool is_fire) {
+            vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+            queue<tup> q;
             int dir[]={-1,0,1,0,-1};
             if(is_fire) {
-                for(int i=0 ; i<a.size() ; ++i) {
-                    for(int j=0 ; j<a[0].size() ; ++j) {
+                for(int i=0 ; i<m ; ++i) {
+                    for(int j=0 ; j<n ; ++j) {
                         if(a[i][j] == 1) {
-                            v[i][j] = 0;
-                            q.push({i, j});
+                            q.push(tup{0, i ,j});
+                            dp[i][j] = 0;
                         }
                     }
                 }
             }
             else {
-                v[0][0] = 0;
-                q.push({0, 0});
+                q.push(tup{0, 0, 0});
+                dp[0][0] = 0;
             }
             while(!q.empty()) {
-                auto [row, col] = q.front();
+                auto [val, r, c] = q.front();
                 q.pop();
                 
                 for(int i=0 ; i<4 ; ++i) {
-                    int r=row+dir[i], c=col+dir[i+1];
-                    if(r>=0 and r<m and c>=0 and c<n and a[r][c]==0 and v[row][col]+1<v[r][c]) {
-                        v[r][c] = 1 + v[row][col];
-                        q.push({r, c});
+                    int x=r+dir[i], y=c+dir[i+1];
+                    if(x>=0 and x<m and y>=0 and y<n and a[x][y]==0 and 1+val<dp[x][y]) {
+                        dp[x][y] = 1 + val;
+                        q.push(tup{dp[x][y], x, y});
                     }
                 }
             }
-            return vector<int>{v[m-1][n-1], v[m-2][n-1], v[m-1][n-2]};
+            return vector<int>{dp[m-1][n-1], dp[m-1][n-2], dp[m-2][n-1]};
         };
-        auto fire=find_corner_three(1), person=find_corner_three(0);
-        if(fire[0] == INT_MAX and person[0] != INT_MAX)
+        auto f=bfs(1), p=bfs(0);
+        if(f[0]==INT_MAX and p[0]!=INT_MAX)
             return 1e9;
-        int diff=fire[0]-person[0];
-        if(person[0] != INT_MAX and diff>=0) {
-            return diff - (fire[1]-person[1]<=diff and fire[2]-person[2]<=diff);
-        }
+        int diff=f[0]-p[0];
+        if(p[0]!=INT_MAX and diff>=0)
+            return diff - (f[1]-p[1]<=diff and f[2]-p[2]<=diff);
         return -1;
     }
 };
