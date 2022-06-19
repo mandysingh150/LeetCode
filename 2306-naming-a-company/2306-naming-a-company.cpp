4a4a3d22ebc07bcc450ac5634bf769ec54d5dd75
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-19 13:28:22 +0530

    Time: 595 ms (93.28%), Space: 83.2 MB (80.70%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
         long long ans=0;
-        unordered_map<char,unordered_set<string>> mp;
+        unordered_set<string> mp[26];
         for(auto i: ideas) {
-            mp[i[0]].insert(i.substr(1));
+            mp[i[0]-'a'].insert(i.substr(1));
         }
         for(char c1='a' ; c1<='z' ; ++c1) {
-            for(char c2='a' ; c2<='z' ; ++c2) {
-                if(c1 != c2) {
-                    int cnt_same=0;
-                    for(auto i: mp[c2]) {
-                        if(mp[c1].count(i)) {
-                            cnt_same++;
-                        }
+            for(char c2=c1+1 ; c2<='z' ; ++c2) {
+                int cnt_same=0;
+                for(auto i: mp[c2-'a']) {
+                    if(mp[c1-'a'].count(i)) {
+                        cnt_same++;
                     }
-                    ans += (mp[c1].size()-cnt_same) * (mp[c2].size()-cnt_same);
                 }
+                ans += 2 * (mp[c1-'a'].size()-cnt_same) * (mp[c2-'a'].size()-cnt_same);
             }
         }
         return ans;
