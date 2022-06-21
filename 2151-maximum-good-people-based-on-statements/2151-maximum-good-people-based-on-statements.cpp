fd5a0d8ac2f90ff5323145cee591bbc7831d918a
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-22 00:24:49 +0530

    Time: 601 ms (17.07%), Space: 8.3 MB (81.30%) - LeetHub

---
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumGood(vector<vector<int>>& a) {
+        int n=a.size(), ans=0;
+        for(int i=0 ; i<(1<<n) ; ++i) {
+            int found=1;
+            for(int j=0 ; j<n ; ++j) {
+                if(i&(1<<j)) {
+                    for(int k=0 ; k<n ; ++k) {
+                        if(j!=k) {
+                            if(a[j][k]==2)
+                                continue;
+                            if(a[j][k] != ((i&(1<<k)) ? 1 : 0)) {
+                                found=0;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if(found)
+                ans = max(ans, __builtin_popcount(i));
+        }
+        return ans;
+    }
+};
