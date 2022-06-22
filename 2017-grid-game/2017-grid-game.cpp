e0fe22f4f4692de1695819a3fdc9decee5f93f0b
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-22 21:01:54 +0530

    Time: 336 ms (8.50%), Space: 62.8 MB (77.55%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    long long gridGame(vector<vector<int>>& a) {
-        long long top_sum = accumulate(begin(a[0]), end(a[0]), 0ll), bottom_sum=0, ans=LLONG_MAX;
-        for(int i=0 ; i<a[0].size() ; ++i) {
-            top_sum -= a[0][i];
-            ans = min(ans, max(top_sum, bottom_sum));
-            bottom_sum += a[1][i];
+    long long gridGame(vector<vector<int>>& grid) {
+        long long row_1_sum = accumulate(begin(grid[0]), end(grid[0]), 0ll), row_2_sum=0ll, ans=LLONG_MAX;
+        for(int i=0 ; i<grid[0].size() ; ++i) {
+            row_1_sum -= grid[0][i];
+            ans = min(ans, max(row_1_sum, row_2_sum));
+            row_2_sum += grid[1][i];
         }
         return ans;
     }
