be29933c80b738fcf5ed4de35549c9b21f3b636e
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-22 17:50:44 +0530

    Time: 122 ms (53.18%), Space: 43.8 MB (24.05%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<int> pq;
+        int a[26]={0};
+        for(auto i: tasks) {
+            a[i-'A']++;
+        }
+        for(int i=0 ; i<26 ; ++i) {
+            if(a[i] > 0) {
+                pq.push(a[i]);
+            }
+        }
+        int all_time=0;
+        while(!pq.empty()) {
+            vector<int> v;
+            int time=0;
+            for(int i=0 ; i<n+1 ; ++i) {
+                if(!pq.empty()) {
+                    v.push_back(pq.top());
+                    pq.pop();
+                    time++;
+                }
+            }
+            for(auto i: v) {
+                if(--i > 0) {
+                    pq.push(i);
+                }
+            }
+            all_time += !pq.empty() ? n+1 : time;
+        }
+        return all_time;
+    }
+};
