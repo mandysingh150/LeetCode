4f2b6783b12e6042d135064c66b5efc840a76d99
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-24 01:12:52 +0530

    Time: 609 ms (15.59%), Space: 63.3 MB (34.25%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -7,16 +7,17 @@ public:
             return c[1] < d[1];
         });
         for(auto i: a) {
+            // if we can take finish the course on or before the last day, then we are taking the course
             if(time + i[0] <= i[1]) {
                 time += i[0];
                 pq.push(i[0]);
             }
-            else {
-                if(!pq.empty() and pq.top() > i[0]) {
-                    time += i[0] - pq.top();
-                    pq.pop();
-                    pq.push(i[0]);
-                }
+            // here, we are trying to minimize the total time of all the taken courses
+            // this is done by replacing a previously taken "longer duration" course with the current course
+            else if(!pq.empty() and pq.top() > i[0]) {
+                time += i[0] - pq.top();
+                pq.pop();
+                pq.push(i[0]);
             }
         }
         return pq.size();
