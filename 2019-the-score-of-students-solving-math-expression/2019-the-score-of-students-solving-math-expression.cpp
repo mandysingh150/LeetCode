9445aab125b3c468ed654b1bc8f81e3af4ddc20c
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-25 17:38:17 +0530

    Time: 1288 ms (45.83%), Space: 165.2 MB (90.62%) - LeetHub

--- 2019-the-score-of-students-solving-math-expression/2019-the-score-of-students-solving-math-expression.cpp
+++ 2019-the-score-of-students-solving-math-expression/2019-the-score-of-students-solving-math-expression.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     unordered_set<int> dp[32][32];
-    void h(vector<int> &exp, int si, int ei) {
+    void h(string &s, int si, int ei) {
         if(dp[si][ei].empty()) {
-            if(si == ei) {
-                dp[si][ei].insert(exp[si]);
-                return;
-            }
+            if(si == ei)
+                dp[si][ei].insert(s[si]-'0');
             for(int i=si+1 ; i<ei ; i+=2) {
-                h(exp, si, i-1);
-                h(exp, i+1, ei);
+                h(s, si, i-1);
+                h(s, i+1, ei);
                 for(auto l_ans: dp[si][i-1]) {
                     for(auto r_ans: dp[i+1][ei]) {
-                        int val = exp[i] == -1111 ? l_ans + r_ans : l_ans * r_ans;
+                        int val = (s[i] == '+' ? l_ans+r_ans : l_ans*r_ans);
                         if(val <= 1000)
                             dp[si][ei].insert(val);
                     }
@@ -21,28 +19,23 @@ public:
         }
     }
     int scoreOfStudents(string &s, vector<int>& answers) {
-        vector<int> exp;
-        for(auto i: s) {
-            exp.push_back((i=='+' ? -1111 : (i=='*' ? 1111 : i-'0')));
-        }
-        
         // getting the correct answer
-        vector<int> v{exp[0]};
-        for(int i=1 ; i<exp.size() ; i+=2) {
-            if(exp[i] == -1111)
-                v.push_back(exp[i+1]);
+        vector<int> v{s[0]-'0'};
+        for(int i=1 ; i<s.size() ; i+=2) {
+            if(s[i] == '+')
+                v.push_back(s[i+1]-'0');
             else
-                v[v.size()-1] *= exp[i+1];
+                v[v.size()-1] *= (s[i+1]-'0');
         }
         int correct_answer = accumulate(begin(v), end(v), 0ll);
         
-        h(exp, 0, exp.size()-1);
+        h(s, 0, s.size()-1);
         
         int val=0;
         for(auto i: answers) {
             if(i == correct_answer)
                 val += 5;
-            else if(dp[0][exp.size()-1].count(i))
+            else if(dp[0][s.size()-1].count(i))
                 val += 2;
         }
         return val;
