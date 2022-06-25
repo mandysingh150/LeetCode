472fb7462b61a225afc52516b9b76424300da8ff
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-26 00:40:41 +0530

    Time: 231 ms (84.81%), Space: 27.4 MB (46.33%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -1,27 +1,24 @@
-long long dp[201][201], mp[201][201];
 class Solution {
 public:
+    long long dp[201][201], price[201][201];
     long long h(int m, int n) {
-        if(m==1 and n==1)
-            return mp[m][n];
         if(dp[m][n] != -1)
             return dp[m][n];
-        
-        long long ans=mp[m][n];
-        for(int i=1 ; i<=m>>1 ; ++i) {
+        long long ans=price[m][n];
+        for(int i=1 ; i<=m/2 ; ++i) {
             ans = max(ans, h(i, n) + h(m-i, n));
         }
-        for(int i=1 ; i<=n>>1 ; ++i) {
+        for(int i=1 ; i<=n/2 ; ++i) {
             ans = max(ans, h(m, i) + h(m, n-i));
         }
         return dp[m][n] = ans;
     }
     long long sellingWood(int m, int n, vector<vector<int>>& prices) {
-        memset(mp, 0, sizeof(mp));
+        memset(price, 0, sizeof(price));
         memset(dp, -1, sizeof(dp));
         for(auto i: prices) {
-            mp[i[0]][i[1]] = i[2];
+            price[i[0]][i[1]] = i[2];
         }
-        return h(m, n);
+        return h(m, n);        
     }
 };
