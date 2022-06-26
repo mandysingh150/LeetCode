95a2866bd6444a8fa5ed5fae92c498b6cf9a6d8e
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-27 01:11:21 +0530

    Time: 213 ms (66.67%), Space: 82.4 MB (100.00%) - LeetHub

---
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int kadane(vector<int> &a, vector<int> &b) {
+        int sum=0, mx=0;
+        for(int i=0 ; i<a.size() ; ++i) {
+            sum = max(b[i]-a[i], sum+(b[i]-a[i]));
+            mx = max(mx, sum);
+        }
+        return mx;
+    }
+    int maximumsSplicedArray(vector<int>& n1, vector<int>& n2) {
+        return max(accumulate(begin(n1), end(n1), 0ll)+kadane(n1, n2), accumulate(begin(n2), end(n2), 0ll)+kadane(n2, n1));
+    }
+};
