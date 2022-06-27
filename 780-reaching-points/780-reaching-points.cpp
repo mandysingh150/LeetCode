330197a7fcd8e00894bd485274fc379897f5dc29
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-06-28 00:11:21 +0530

    Time: 3 ms (38.57%), Space: 6 MB (14.29%) - LeetHub

--- 780-reaching-points/780-reaching-points.cpp
+++ 780-reaching-points/780-reaching-points.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    unordered_map<string,bool> mp;
     bool reachingPoints(int sx, int sy, int tx, int ty) {
-        if(sx>tx or sy>ty)
-            return 0;
-        if(sx==tx)
-            return (ty-sy)%sx==0;
-        if(sy==ty)
-            return (tx-sx)%sy==0;
-        
-        if(tx > ty)
-            return reachingPoints(sx, sy, tx%ty, ty);
-        else if(tx < ty)
-            return reachingPoints(sx, sy, tx, ty%tx);
-        return 0;
+        // nice visualization by "nits2010" - https://leetcode.com/problems/reaching-points/discuss/375429/Detailed-explanation.-or-full-through-process-or-Java-100-beat
+        // there is a trivial path in recursion tree when we move from (tx, ty) to (sx, sy)
+        while(sx<tx and sy<ty)
+            if(tx<ty) 
+                ty%=tx;
+            else 
+                tx%=ty;
+        return (sx==tx and sy<=ty and (ty-sy)%sx==0) or (sy==ty and sx<=tx and (tx-sx)%sy==0);
     }
 };
