b43ea35eea36c6fa0f1b2c358104d2c948e48278
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-01 22:41:47 +0530

    Time: 36 ms (67.99%), Space: 26.9 MB (81.32%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& a) {
-        int cnt_dips=0;
+        int min_steps=0;
         for(int i=1 ; i<a.size() ; ++i) {
             if(a[i-1] > a[i]) {
-                if(i-2<0) {
-                    // as 'i-1' is the 0th index, we can make array non-decreasing by changing a[i-1], iff a[1] to a[n-1] is non-decreasing
-                    a[i-1]=a[i];
-                }
-                else if(a[i-2]<=a[i]) {
-                    // here (a[i-2] <= a[i-1] >= a[i]), we can make it no-decreasing by changing a[i-1] to a[i]
-                    a[i-1]=a[i];
-                }
-                else {
-                    // here (a[i-2] <= a[i-1] >= a[i]) and (a[i-2] > a[i]), so we can make it non-decreasing by changing a[i] to a[i-1]
+                if(i-2>=0 and a[i-2]>a[i]) {
                     a[i]=a[i-1];
                 }
-                cnt_dips++;
+                else {
+                    a[i-1]=a[i];
+                }
+                min_steps++;
             }
         }
-        return cnt_dips<=1;
+        return min_steps<=1;
     }
 };
