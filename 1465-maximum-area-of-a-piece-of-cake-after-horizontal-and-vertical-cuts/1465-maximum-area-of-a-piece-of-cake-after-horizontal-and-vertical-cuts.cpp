eb36cf8f8cca95bcc116b3b51ca51399790796d9
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-02 23:16:43 +0530

    Time: 88 ms (76.56%), Space: 34.6 MB (6.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int maxArea(int ht, int wid, vector<int>& hc, vector<int>& vc) {
-        int h_mx=0, v_mx=0, cnt=0;
+        long long h_mx=0, v_mx=0, cnt=0;
+        hc.push_back(ht);
+        vc.push_back(wid);
         for(auto a: {hc, vc}) {
-            int val = 0, t=(cnt ? wid : ht);
             sort(begin(a), end(a));
-            for(int i=0 ; i<=a.size() ; ++i) {
-                if(i == 0) {
-                    val = max(val, a[i]);
-                }
-                else if(i == a.size()) {
-                    val = max(val, t-a[i-1]);
-                }
-                else {
-                    val = max(val, a[i]-a[i-1]);
-                }
+            int val = a[0], t=(cnt ? wid : ht);
+            for(int i=1 ; i<a.size() ; ++i) {
+                val = max(val, a[i]-a[i-1]);
             }
             (cnt==1 ? v_mx : h_mx) = val;
             cnt++;
         }
-        return (1ll*h_mx*v_mx)%(int(1e9+7));
+        return (h_mx*v_mx)%(int(1e9+7));
     }
 };
