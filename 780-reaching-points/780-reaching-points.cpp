7542c2e383f7f6bff188a10ebebd50ab9bf36983
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-04 18:10:26 +0530

    Time: 6 ms (9.16%), Space: 6 MB (14.90%) - LeetHub

--- 780-reaching-points/780-reaching-points.cpp
+++ 780-reaching-points/780-reaching-points.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool reachingPoints(int sx, int sy, int tx, int ty) {
-        // nice visualization by "nits2010" - https://leetcode.com/problems/reaching-points/discuss/375429/Detailed-explanation.-or-full-through-process-or-Java-100-beat
-        // there is a trivial path in recursion tree when we move from (tx, ty) to (sx, sy)
-        while(sx<tx and sy<ty)
-            if(tx<ty) 
-                ty%=tx;
-            else 
-                tx%=ty;
-        return (sx==tx and sy<=ty and (ty-sy)%sx==0) or (sy==ty and sx<=tx and (tx-sx)%sy==0);
+        if(tx<sx or ty<sy) {
+            return 0;
+        }
+        if(sx == tx) {
+            return (ty-sy)%sx==0;
+        }
+        if(sy == ty) {
+            return (tx-sx)%sy==0;
+        }
+        return tx>=ty ? reachingPoints(sx, sy, tx%ty, ty) : reachingPoints(sx, sy, tx, ty%tx);
     }
 };
