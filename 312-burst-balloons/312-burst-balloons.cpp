b97ffe1f181f545a9f13a4736828b4fe16409562
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-04 23:31:37 +0530

    Time: 563 ms (61.05%), Space: 8.2 MB (94.19%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int mp[500][500];
-    int h(vector<int>& a, int si, int ei) {
-        if(si>ei)
+    int dp[301][301];
+    int h(vector<int> &a, int si, int ei) {
+        if(si > ei) {
             return 0;
-        if(mp[si][ei] != -1)
-            return mp[si][ei];
-        
-        int leftValue = si-1<0 ? 1 : a[si-1];
-        int rightValue = ei+1>=a.size() ? 1 : a[ei+1];
-        int maxValue = 0;
-        for(int i=si ; i<=ei ; ++i) {
-            int left_ans = h(a, si, i-1);
-            int right_ans = h(a, i+1, ei);
-            maxValue = max(maxValue, (leftValue*a[i]*rightValue) + left_ans + right_ans);
         }
-        return mp[si][ei] = maxValue;
+        if(si == ei) {
+            return (si-1<0 ? 1 : a[si-1]) * a[si] * (ei+1==a.size() ? 1 : a[ei+1]);
+        }
+        if(dp[si][ei] != -1)
+            return dp[si][ei];
+        int mx=0;
+        for(int i=si ; i<=ei ; ++i) {
+            mx = max(mx, (si-1<0 ? 1 : a[si-1]) * a[i] * (ei+1==a.size() ? 1 : a[ei+1]) + h(a, si, i-1) + h(a, i+1, ei));
+        }
+        return dp[si][ei] = mx;
     }
-    int maxCoins(vector<int>& a) {
-        memset(mp, -1, sizeof(mp));
-        return h(a, 0, a.size()-1);
+    int maxCoins(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return h(nums, 0, nums.size()-1);
     }
 };
