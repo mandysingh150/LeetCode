4f667a88b2faf73079eed3c7569fd96f234d7e4b
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-05 17:46:23 +0530

    Time: 28 ms (68.12%), Space: 10.2 MB (32.18%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.cpp
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    int worker[12] = {}, res = 0;
-    int dfs(vector<int>& jobs, int i, int k, int cur) {
-        if (cur >= res)
-            return res;
-        if (i == jobs.size())
-            return res = cur;
-        unordered_set<int> workTime;
-        for (auto j = 0; j < k; ++j) {
-            if (!workTime.insert(worker[j]).second)
-                continue;
-            worker[j] += jobs[i];
-            dfs(jobs, i + 1, k, max(cur, worker[j]));
-            worker[j] -= jobs[i];
+    int mn, a[12];
+    void h(vector<int> &v, int in, int k, int mx) {
+        if(mx >= mn)
+            return;
+        if(in == v.size()) {
+            mn = min(mn, mx);
+            return;
         }
-        return res;
+        unordered_set<int> s;
+        for(int i=0 ; i<k ; ++i) {
+            if(s.count(a[i])) {
+                continue;
+            }
+            if(a[i]+v[in] >= mn) {
+                continue;
+            }
+            
+            s.insert(a[i]);
+            a[i] += v[in];
+            h(v, in+1, k, max(mx, a[i]));
+            a[i] -= v[in];
+        }
     }
     int minimumTimeRequired(vector<int>& jobs, int k) {
-        if (k == jobs.size())
+        if(k == jobs.size()) {
             return *max_element(begin(jobs), end(jobs));
+        }
         sort(begin(jobs), end(jobs), greater<int>());
-        for (int i = 0; i < jobs.size(); i += k)
-            res += jobs[i];
-        return dfs(jobs, 0, k, 0);
+        mn=0;
+        for(auto &i: jobs) mn += i;
+        memset(a, 0, sizeof(a));
+        h(jobs, 0, k, 0);
+        return mn;
     }
 };
