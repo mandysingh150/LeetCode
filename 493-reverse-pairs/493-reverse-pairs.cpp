c255612d6a1453f7aa1df21aac7aa6bb5e008509
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-08 17:43:21 +0530

    Time: 689 ms (59.64%), Space: 45.1 MB (95.47%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,8 +1,11 @@
-int b[50005];
 class Solution {
 public:
-    int merge(vector<int> &a, int si, int mid, int ei) {
-        int i=si, j=mid+1, k=0, cnt=0;
+    int merge_sort(vector<int> &a, int si, int ei) {
+        if(si >= ei)
+            return 0;
+        int mid = (si+ei)>>1;
+        int cnt = merge_sort(a, si, mid) + merge_sort(a, mid+1, ei);
+        int i=si, j=mid+1;
         while(i<=mid and j<=ei) {
             if(a[i] > 2ll*a[j]) {
                 cnt += mid-i+1;
@@ -12,30 +15,8 @@ public:
                 i++;
             }
         }
-        i=si, j=mid+1;
-        while(i<=mid and j<=ei) {
-            if(a[i] <= a[j]) b[k]=a[i++];
-            else b[k]=a[j++];
-            k++;
-        }
-        while(i<=mid) {
-            b[k]=a[i++];
-            k++;
-        }
-        while(j<=ei) {
-            b[k]=a[j++];
-            k++;
-        }
-        for(int t=0 ; t<k ; ++t) {
-            a[si+t] = b[t];
-        }
+        sort(begin(a)+si, begin(a)+ei+1);
         return cnt;
-    }
-    int merge_sort(vector<int> &a, int si, int ei) {
-        if(si >= ei)
-            return 0;
-        int mid = (si+ei)>>1;
-        return merge_sort(a, si, mid) + merge_sort(a, mid+1, ei) + merge(a, si, mid, ei);
     }
     int reversePairs(vector<int>& nums) {
         return merge_sort(nums, 0, nums.size()-1);
