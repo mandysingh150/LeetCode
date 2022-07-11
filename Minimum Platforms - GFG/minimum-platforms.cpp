59f3257302086da4d17bc7c6591138585dedceb7
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-11 13:41:07 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -12,22 +12,19 @@ class Solution{
     //railway station such that no train waits.
     int findPlatform(int at[], int dt[], int n)
     {
-    	vector<pair<int,int>> v(2*n);
-        for(int i=0 ; i<n ; i++) {
-            v[2*i] = {at[i], 0};
-            v[2*i+1] = {dt[i]+1, 1};
-        }
-        sort(v.begin(), v.end());
-        int cnt=0, mx=0;
-        for(int i=0 ; i<v.size() ; ) {
-            int val=v[i].first, temp=v[i].second;
-            i++;
-            while(i<v.size() and v[i].first==val) {
-                cnt += (v[i].second ? -1 : 1);
-                i++;            
+    	sort(at, at+n);
+        sort(dt, dt+n);
+        int cnt=0, mx=0, i=0, j=0;
+        while(i<n and j<n) {
+            if(at[i] <= dt[j]) {
+                i++;
+                cnt++;
             }
-            cnt += (temp ? -1 : 1);
-            mx = max(mx, cnt);
+            else {
+                j++;
+                cnt--;
+            }
+            mx = max(cnt, mx);
         }
         return mx;
     }
