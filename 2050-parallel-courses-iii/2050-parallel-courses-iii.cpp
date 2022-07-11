6ab1e0c3d2495d877cfb3b31a54d1856fce24966
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-11 10:09:37 +0530

    Time: 901 ms (14.81%), Space: 158.4 MB (13.72%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     unordered_map<int,vector<int>> mp;
-    int dfs(int node, vector<int> &time, vector<int> &dist) {
-        if(dist[node] != -1) {
-            return dist[node];
-        }
-        int ans = time[node-1];
-        for(auto ng: mp[node]) {
-            ans = max(ans, time[node-1] + dfs(ng, time, dist));
-        }
-        return dist[node] = ans;
-    }
     int minimumTime(int n, vector<vector<int>>& a, vector<int>& time) {
-        vector<int> indegree(n+1, 0);
+        vector<int> indegree(n, 0);
         for(auto i: a) {
-            mp[i[0]].push_back(i[1]);
-            indegree[i[1]]++;
+            mp[i[0]-1].push_back(i[1]-1);
+            indegree[i[1]-1]++;
         }
-        vector<int> v, dist(n+1, -1);
-        for(int i=1 ; i<=n ; ++i) {
+        queue<int> q;
+        vector<int> dist(n, 0);
+        for(int i=0 ; i<n ; ++i) {
             if(indegree[i] == 0) {
-                v.push_back(i);
+                q.push(i);
+                dist[i] += time[i];
             }
         }
-        int ans=0;
-        for(auto ele: v) {
-            ans = max(ans, dfs(ele, time, dist));
+        while(!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            
+            for(auto ng: mp[node]) {
+                dist[ng] = max(dist[node] + time[ng], dist[ng]);
+                if(--indegree[ng] == 0) {
+                    q.push(ng);
+                }
+            }
         }
-        return ans;
+        return *max_element(begin(dist), end(dist));
     }
 };
