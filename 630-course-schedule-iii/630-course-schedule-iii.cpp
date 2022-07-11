e9ee9dc4f47b0c524a29833449bd22aa20f94b4d
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-11 09:34:38 +0530

    Time: 384 ms (62.24%), Space: 56.3 MB (75.48%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int scheduleCourse(vector<vector<int>>& courses) {
-        int time=0;
+    int scheduleCourse(vector<vector<int>>& a) {
         priority_queue<int> pq;
-        sort(begin(courses), end(courses), [](vector<int> &c, vector<int>&d) {
+        int time=0;
+        sort(begin(a), end(a), [](vector<int> &c, vector<int> &d) {
+            if(c[1] == d[1])
+                return c[0]<d[0];
             return c[1]<d[1];
         });
-        for(auto i: courses) {
-            if(time + i[0] <= i[1]) {
-                time += i[0];
-                pq.push(i[0]);
+        for(int i=0; i<a.size(); ++i) {
+            if(time + a[i][0] <= a[i][1]){
+                time += a[i][0];
+                pq.push(a[i][0]);
             }
-            else if(!pq.empty() and i[0] < pq.top()){
-                time += i[0] - pq.top();
+            else if(!pq.empty() and a[i][0]<pq.top()) {
+                time += a[i][0] - pq.top();
                 pq.pop();
-                pq.push(i[0]);
+                pq.push(a[i][0]);
             }
         }
         return pq.size();
