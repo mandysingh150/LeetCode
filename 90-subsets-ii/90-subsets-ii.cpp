7c234bf6fc66dbe554558f2f642389749e5b0920
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-12 19:40:50 +0530

    Time: 4 ms (69.34%), Space: 7.7 MB (60.98%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void h(vector<int> &a, int in, vector<int> &v, set<vector<int>> &s) {
-        if(a.size()==in) {
-            s.insert(v);
-            return;
+    vector<vector<int>> s;
+    void h(vector<int> &a, int in, vector<int> &v) {
+        s.push_back(v);
+        for(int i=in ; i!=a.size() ; ++i) {
+            if(i==in || a[i] != a[i-1]) {
+                v.push_back(a[i]);
+                h(a, i+1, v);
+                v.pop_back();
+            }
         }
-        h(a, in+1, v, s);
-        v.push_back(a[in]);
-        h(a, in+1, v, s);
-        v.pop_back();
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> s;
         vector<int> v;
-        h(nums, 0, v, s);
-        return vector<vector<int>>(s.begin(), s.end());
+        h(nums, 0, v);
+        return s;
     }
 };
