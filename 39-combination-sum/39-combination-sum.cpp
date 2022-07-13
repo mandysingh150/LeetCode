e6524b826e11dee505601d2f1fe02c71789daa3d
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-13 13:36:10 +0530

    Time: 16 ms (50.49%), Space: 11.6 MB (48.35%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> s;
-    void h(vector<int> &a, int x, int in, vector<int> &v) {
-        if(in == a.size() or x == 0) {
+    vector<vector<int>> ans;
+    void h(vector<int> &a, int in, int x, vector<int> &t) {
+        if(x <= 0) {
             if(x == 0) {
-                s.push_back(v);
+                ans.push_back(t);
             }
             return;
         }
-        for(int i=0 ; i*a[in]<=x ; ++i) {
-            if(i>0) {
-                v.push_back(a[in]);
-            }
-            h(a, x-(i*a[in]), in+1, v);
+        if(in == a.size()) {
+            return;
         }
-        while(!v.empty() and v.back() == a[in])
-            v.pop_back();
+        int i=0;
+        while(i*a[in]<=x) {
+            h(a, in+1, x-a[in]*i, t);
+            t.push_back(a[in]);
+            i++;
+        }
+        while(i--) {
+            t.pop_back();
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& a, int x) {
-        s.clear();
-        vector<int> v;
-        h(a, x, 0, v);
-        return s;
+        vector<int> t;
+        h(a, 0, x, t);
+        return ans;
     }
 };
