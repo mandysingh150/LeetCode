3adaccfc44ebe82727ee2ccc6dcfbd67e5240479
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-13 14:05:43 +0530

    Time: 3 ms (95.23%), Space: 10.9 MB (37.57%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> v;
+    void h(vector<int> &a, int in, int x, vector<int> &t) {
+        if(x == 0) {
+            v.push_back(t);
+        }
+        if(in == a.size()) {
+            
+            return;
+        }
+        for(int i=in ; i<a.size() and a[i]<=x ; ++i) {
+            if(i==in or a[i]!=a[i-1]) {
+                t.push_back(a[i]);
+                h(a, i+1, x-a[i], t);
+                t.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& a, int x) {
+        sort(begin(a), end(a));
+        vector<int> t;
+        h(a, 0, x, t);
+        return v;
+    }
+};
