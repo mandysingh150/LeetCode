02bc7dfd807a1ca361da2986a25bd69fd95a06d4
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-15 18:20:39 +0530

    Time: 44 ms (43.40%), Space: 22.3 MB (55.04%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -6,21 +6,11 @@ public:
         int l=0, h=a.size()-1;
         while(l<=h) {
             int mid=(l+h)>>1;
-            if(mid&1) {
-                if(a[mid]==a[mid-1]) {
-                    l=mid+1;
-                }
-                else {
-                    h=mid-1;
-                }
+            if(a[mid]==a[mid^1]) {
+                l=mid+1;
             }
             else {
-                if(a[mid]==a[mid+1]) {
-                    l=mid+1;
-                }
-                else {
-                    h=mid-1;
-                }
+                h=mid-1;
             }
         }
         return a[l];
