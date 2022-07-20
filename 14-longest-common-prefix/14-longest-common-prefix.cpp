6683ab26d7e853f968094b83ad5d5783b617bede
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-20 22:55:40 +0530

    Time: 8 ms (43.77%), Space: 9.7 MB (13.40%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string s="";
-        for(int i=0 ; i<strs[0].size() ; ++i) {
-            char ch=strs[0][i];
-            bool same=1;
-            for(auto str: strs) {
-                if(i<str.size() and str[i]==ch)
-                    continue;
-                same=0;
-                break;
+    class trienode {
+    public:
+        int cnt=0;
+        trienode* children[26];
+        trienode() {
+            cnt=0;
+            for(int i=0 ; i<26 ; ++i) {
+                children[i]=nullptr;
             }
-            if(same)
-                s.push_back(ch);
-            else
-                break;
         }
-        return s;
+    };
+    
+    void add_word(trienode *r, string &s, int in) {
+        if(in == s.size()) {
+            return;
+        }
+        if(!r->children[s[in]-'a']) {
+            r->children[s[in]-'a'] = new trienode();
+        }
+        r->children[s[in]-'a']->cnt++;
+        add_word(r->children[s[in]-'a'], s, in+1);
+    }
+    
+    string longestCommonPrefix(vector<string>& strs) {
+        trienode *root = new trienode();
+        for(auto &i: strs) add_word(root, i, 0);
+        string ans="";
+        for(int i=0 ; i<26 ; ++i) {
+            if(root->children[i]) {
+                if(root->children[i]->cnt < strs.size()) {
+                    return ans;
+                }
+                ans.push_back('a'+i);
+                root = root->children[i];
+                i=-1;
+            }
+        }
+        return ans;
     }
 };
