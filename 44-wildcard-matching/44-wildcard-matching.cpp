b36de7b996f043a10c846ffb4c723a322e933153
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-23 00:39:21 +0530

    Time: 1422 ms (5.01%), Space: 22 MB (45.31%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,23 +1,20 @@
 int dp[2005][2005];
 class Solution {
 public:
-    bool h(string &s, string &p, int in_s, int in_p) {
-        if(in_p >= p.size())
+    bool isMatch(string &s, string &p, int in_s, int in_p) {
+        if(in_p >= p.size()) {
             return in_s >= s.size();
-
-        if(dp[in_s][in_p] != -1)
+        }
+        if(dp[in_s][in_p] != -1) {
             return dp[in_s][in_p];
-        
-        if(in_s<s.size() and (p[in_p]==s[in_s] or p[in_p]=='?'))
-            return h(s, p, in_s+1, in_p+1);
-        
-        if(p[in_p] == '*')
-            return dp[in_s][in_p] = h(s, p, in_s, in_p+1) or in_s<s.size() and h(s, p, in_s+1, in_p);
-            
-        return 0;
+        }
+        if(in_s<s.size() and (s[in_s]==p[in_p] or p[in_p]=='?')) {
+            return dp[in_s][in_p] = isMatch(s, p, in_s+1, in_p+1);
+        }
+        return dp[in_s][in_p] = (p[in_p]=='*' and (isMatch(s, p, in_s, in_p+1) or in_s<s.size() and isMatch(s, p, in_s+1, in_p)));
     }
     bool isMatch(string &s, string &p) {
         memset(dp, -1, sizeof(dp));
-        return h(s, p, 0, 0);
+        return isMatch(s, p, 0, 0);
     }
 };
