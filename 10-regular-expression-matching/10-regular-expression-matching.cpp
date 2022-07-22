775a46d5abc1124d9d061c7d5f9d985afd8fdec2
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-23 00:25:42 +0530

    Time: 3 ms (91.63%), Space: 6.1 MB (98.50%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,13 +1,21 @@
+int dp[21][31];
 class Solution {
 public:
-    bool isMatch(string &s, string &p, int in_s=0, int in_p=0) {
+    bool isMatch(string &s, string &p, int in_s, int in_p) {
         if(in_p >= p.size()) {
             return in_s >= s.size();
         }
+        if(dp[in_s][in_p] != -1) {
+            return dp[in_s][in_p];
+        }
         bool ans = (in_s<s.size() and (s[in_s]==p[in_p] or p[in_p]=='.'));
         if(in_p+1<p.size() and p[in_p+1]=='*') {
-            return (isMatch(s, p, in_s, in_p+2) or (ans and isMatch(s, p, in_s+1, in_p)));
+            return dp[in_s][in_p] = (isMatch(s, p, in_s, in_p+2) or (ans and isMatch(s, p, in_s+1, in_p)));
         }
-        return ans and isMatch(s, p, in_s+1, in_p+1);
+        return dp[in_s][in_p] = ans and isMatch(s, p, in_s+1, in_p+1);
+    }
+    bool isMatch(string &s, string &p) {
+        memset(dp, -1, sizeof(dp));
+        return isMatch(s, p, 0, 0);
     }
 };
