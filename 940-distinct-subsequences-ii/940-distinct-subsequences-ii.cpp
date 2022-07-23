2d770b6209a73ba4c3e275deb670d11157f364b7
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-23 18:18:24 +0530

    Time: 12 ms (35.02%), Space: 6.4 MB (99.09%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -2,15 +2,8 @@
 class Solution {
 public:
     int distinctSubseqII(string &s) {
-        int dp[2005], last_occured[26];
-        memset(last_occured, -1, sizeof(last_occured));
-        dp[0]=1;
-        for(int i=1 ; i<=s.size() ; ++i) {
-            dp[i] = ((2ll * dp[i-1])%mod - (last_occured[s[i-1]-'a']>=0 ? dp[last_occured[s[i-1]-'a']] : 0))%mod;
-            last_occured[s[i-1]-'a'] = i-1;
-        }
-        if(--dp[s.size()] < 0)
-            dp[s.size()] += mod;
-        return dp[s.size()];
+        long long ends_with[26]={0};
+        for(auto ch: s) ends_with[ch-'a'] = accumulate(begin(ends_with), end(ends_with), 1ll) % mod;
+        return accumulate(begin(ends_with), end(ends_with), 0ll) % mod;
     }
 };
