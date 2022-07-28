d8b2f7eac1147ed72d2383cb70fc2dffbe8b0d26
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-28 14:06:52 +0530

    Time: 827 ms (50.30%), Space: 211.6 MB (30.67%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    vector<int> v;
-    void merge_sort(vector<pair<int,int>> &a, int si, int ei) {
-        if(si >= ei) {
-            return;
-        }
+    vector<int> ans;
+    void help(vector<pair<int,int>> &a, int si, int ei) {
+        if(si >= ei) return;
         int mid = (si+ei)/2;
-        merge_sort(a, si, mid);
-        merge_sort(a, mid+1, ei);
+        help(a, si, mid);
+        help(a, mid+1, ei);
         
-        // merge in descending order and count the inversions to fill the answer vector
+        // merge in descending order and update the answer array
         vector<pair<int,int>> t(ei-si+1);
         int i=si, j=mid+1, k=0;
         while(i<=mid and j<=ei) {
@@ -17,27 +15,23 @@ public:
                 t[k++] = a[j++];
             }
             else {
-                v[a[i].second] += (ei-j+1);
+                ans[a[i].second] += ei-j+1;
                 t[k++] = a[i++];
             }
         }
-        while(i<=mid) {
-            t[k++] = a[i++];
-        }
-        while(j<=ei) {
-            t[k++] = a[j++];
-        }
+        while(i<=mid) t[k++] = a[i++];
+        while(j<=ei) t[k++] = a[j++];
         for(int i=si ; i<=ei ; ++i) {
             a[i] = t[i-si];
         }
     }
     vector<int> countSmaller(vector<int>& a) {
-        v = vector<int>(a.size(), 0);
-        vector<pair<int,int>> arr(a.size());
+        ans = vector<int>(a.size(), 0);
+        vector<pair<int,int>> v(a.size());
         for(int i=0 ; i<a.size() ; ++i) {
-            arr[i] = {a[i], i};
+            v[i] = {a[i], i};
         }
-        merge_sort(arr, 0, a.size()-1);
-        return v;
+        help(v, 0, a.size()-1);
+        return ans;
     }
 };
