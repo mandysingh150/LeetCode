d0b4d3ec76b8c8c2a9ac78be690ace9b7ca2ec7b
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-28 12:58:22 +0530

    Time: 10 ms (74.53%), Space: 7 MB (99.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    bool isAnagram(string &s, string &t) {
         if(s.size() != t.size()) return 0;
-        int a[256]={0};
-        for(auto i: s) a[i]++;
-        for(auto i: t) if(--a[i] < 0) return 0;
+        int cnt[26]={0};
+        for(int i=0 ; i<s.size() ; ++i) cnt[s[i]-'a']++;
+        for(int i=0 ; i<t.size() ; ++i) if(--cnt[t[i]-'a'] < 0) return 0;
         return 1;
     }
 };
