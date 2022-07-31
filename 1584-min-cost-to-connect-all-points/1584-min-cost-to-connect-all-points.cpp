7860016bcd4613d9d2cf848a07edbf43c388cfc9
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-07-31 22:47:22 +0530

    Time: 321 ms (72.09%), Space: 42.2 MB (77.62%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    
-    int getParent(vector<int>& parents, int idx) {
-        return parents[idx]==idx ? idx : parents[idx]=getParent(parents, parents[parents[idx]]);
-    }
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<int> parents(n);        
-        iota(parents.begin(), parents.end(), 0);
-        int ans = 0;
-        int edges = 0;
-        priority_queue<vector<int>> pq;
-
-        for(int i=0;i<n;i++)
-            for(int j=i+1;j<n;j++)
-                if(i!=j)
-                     pq.push({-1*(abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])), i, j});
-        while(edges!=n-1) {
-            vector<int> edge = pq.top();
-            int p1 = getParent(parents, edge[1]);
-            int p2 = getParent(parents, edge[2]);
-            
-            if(p1!=p2){
-                ans += -1*edge[0];
-                parents[p1] = p2;
-                edges++;   
-            }
+    int minCostConnectPoints(vector<vector<int>>& ps) {
+        int n = ps.size(), res = 0, i = 0, connected = 0;
+        vector<bool> visited(n);
+        priority_queue<pair<int, int>> pq;
+        while (++connected < n) {
+            visited[i] = true;
+            for (int j = 0; j < n; ++j)
+                if (!visited[j])
+                    pq.push({-(abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1])), j});
+            while (visited[pq.top().second])
+                pq.pop();
+            res -= pq.top().first;
+            i = pq.top().second;
             pq.pop();
-            
         }
-        return ans;
+        return res;
     }
 };
