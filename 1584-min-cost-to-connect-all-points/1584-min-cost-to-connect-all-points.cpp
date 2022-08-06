a1a3f0e4d76ccbec88de46b28b3218c3c102489b
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-08-06 19:23:53 +0530

    Time: 207 ms (85.57%), Space: 42.2 MB (77.81%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    int find(vector<int> &p, int node) {
-        return p[node] < 0 ? node : p[node] = find(p, p[node]);
-    }
     int minCostConnectPoints(vector<vector<int>>& a) {
-        // implementing Kruskal's algorithm
-        int n=a.size(), ans=0;
-        // if dsu[i]==-ve, then abs(dsu[i]) gives the tree size of node 'i'
-        vector<int> dsu(n, -1);
-        vector<array<int,3>> v;
-        for(int i=0 ; i<n ; ++i) {
-            for(int j=i+1 ; j<n ; ++j) {
-                v.push_back({abs(a[i][0]-a[j][0]) + abs(a[i][1]-a[j][1]), i, j});
-            }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        int ans=0, cnt=1;
+        vector<bool> vis(a.size(), 0);
+        vis[0] = 1;
+        for(int j=1 ; j<a.size() ; ++j) {
+            pq.push({abs(a[0][0]-a[j][0]) + abs(a[0][1]-a[j][1]), j});
         }
-        make_heap(begin(v), end(v), greater<array<int,3>>());
-        while(!v.empty()) {
-            pop_heap(begin(v), end(v), greater<array<int,3>>());
-            auto [distance, n1, n2] = v.back();
-            v.pop_back();
-            n1 = find(dsu, n1), n2 = find(dsu, n2);
-            if(n1 != n2) {
-                // take union
-                ans += distance;
-                
-                // abs(dsu[n1]) nodes will be added to the tree with parent 'n2'
-                dsu[n2] += dsu[n1];
-                // changing parent of tree 'n1' to 'n2'
-                dsu[n1] = n2;
-                
-                if(dsu[n2] == -n) {
-                    break;
+        while(!pq.empty() and cnt<a.size()) {
+            while(vis[pq.top().second]) pq.pop();
+            auto [dist, in] = pq.top();
+            pq.pop();
+            
+            vis[in] = 1;
+            ans += dist;
+            cnt++;
+            
+            for(int j=0 ; j<a.size() ; ++j) {
+                if(!vis[j]) {
+                    pq.push({abs(a[in][0]-a[j][0]) + abs(a[in][1]-a[j][1]), j});
                 }
             }
         }
