39240f71672b124d709cad5d5411a28563e4449d
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-08-10 13:47:23 +0530

    Time: 11 ms (80.22%), Space: 5.9 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -3,26 +3,22 @@ class Solution {
 public:
     int countVowelPermutation(int n) {
         // 0=a, 1=e, 2=i, 3=o, 4=u
-        int dp[2][5];
-        for(int i=0 ; i<2 ; ++i) {
-            for(int j=0 ; j<5 ; ++j) {
-                dp[i][j] = 1;
+        int dp[2][5], mp[6][6] = {{1, 1}, {2, 0, 2}, {4, 0, 1, 3, 4}, {2, 2, 4}, {1, 0}};        
+        for(int i=n-1 ; i>=0 ; --i) {
+            if(i == n-1) {
+                for(int j=0 ; j<5 ; ++j) {
+                    dp[i%2][j] = 1;
+                }
             }
-        }
-        int mp[6][6] = {{1, 1}, {2, 0, 2}, {4, 0, 1, 3, 4}, {2, 2, 4}, {1, 0}};
-        
-        for(int i=n-2 ; i>=0 ; --i) {
-            for(int j=0 ; j<5 ; ++j) {
-                dp[i%2][j] = 0;
-                for(int in=1 ; in<=mp[j][0] ; ++in) {
-                    dp[i%2][j] = (0ll + dp[i%2][j] + dp[(i+1)%2][mp[j][in]])%mod;
+            else {
+                for(int j=0 ; j<5 ; ++j) {
+                    dp[i%2][j] = 0;
+                    for(int in=1 ; in<=mp[j][0] ; ++in) {
+                        dp[i%2][j] = (0ll + dp[i%2][j] + dp[(i+1)%2][mp[j][in]])%mod;
+                    }
                 }
             }
         }
-        int sum = 0;
-        for(int i=0 ; i<5 ; ++i) {
-            sum = (0ll + sum + dp[0][i]) % mod;
-        }
-        return sum;
+        return accumulate(dp[0], dp[0]+5, 0ll)%mod;
     }
 };
