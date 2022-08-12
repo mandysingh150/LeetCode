312928349fcd009b9aa6bf0ef8d9498f65cafd7d
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-08-12 15:59:59 +0530

    Time: 3 ms (40.36%), Space: 5.7 MB (99.64%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -3,7 +3,7 @@ public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         int cnt=0, multiple=(minutesToTest/minutesToDie)+1, val=1;
         while(val < buckets) {
-            val *= 1ll*multiple;
+            val *= multiple;
             cnt++;
         }
         return cnt;
