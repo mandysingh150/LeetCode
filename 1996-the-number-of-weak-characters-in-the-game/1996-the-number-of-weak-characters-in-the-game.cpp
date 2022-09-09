a83b03cb8def1c75f27c396dcde8640c473bae8b
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-09-09 09:18:13 +0530

    Time: 2315 ms (5.05%), Space: 181.5 MB (5.04%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -4,7 +4,9 @@ public:
         sort(begin(a), end(a));
         unordered_map<int,int> mp;
         int mx=0, cnt=0;
-        for(int i=a.size()-1 ; i>=0 ; --i) mx = mp[a[i][0]] = max(mp[a[i][0]], max(mx, a[i][1]));
+        for(int i=a.size()-1 ; i>=0 ; --i) {
+            mx = mp[a[i][0]] = max(mp[a[i][0]], max(mx, a[i][1]));
+        }
         for(auto i: a) {
             auto in = lower_bound(begin(a), end(a), vector<int>{i[0]+1, i[1]+1}) - begin(a);
             cnt += (in < a.size() and mp[a[in][0]] > i[1]);
