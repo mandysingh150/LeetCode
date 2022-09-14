17a5f2896d09c1dc61467cdb128d1eb2668b66e4
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-09-14 10:24:57 +0530

    Time: 643 ms (44.76%), Space: 179.6 MB (82.62%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,27 +11,15 @@
  */
 class Solution {
 public:
-    // int a[10], 
     int cnt=0;
     void h(TreeNode *root, int &val) {
         if(!root) return;
-        // a[root->val]++;
         val ^= (1<<(root->val));
-        if(!root->left and !root->right) {
-            cnt += (val==0 or (val&(val-1)) == 0);
-            // int odd_cnt=0;
-            // for(int i=0 ; i<10 ; ++i) odd_cnt += (a[i]&1);
-            // if(odd_cnt<=1) cnt++;
-        }
-        else {
-            h(root->left, val);
-            h(root->right, val);
-        }
-        // a[root->val]--;
+        if(!root->left and !root->right) cnt += (val==0 or (val&(val-1)) == 0);
+        else h(root->left, val), h(root->right, val);
         val ^= (1<<(root->val));
     }
     int pseudoPalindromicPaths (TreeNode* root) {
-        // memset(a, 0, sizeof(a));
         cnt=0;
         int val=0;
         h(root, val);
