a868b3ee64b825fa1cf56cf59879684a44a7be62
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-09-16 17:44:12 +0530

    Time: 925 ms (22.84%), Space: 335.6 MB (5.74%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,15 @@
+// int dp[1005][1005];
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int h(vector<int> &a, vector<int> &b, int in, int si, int ei) {
+        if(in == b.size())
+            return 0;
+        if(dp[in][si] != INT_MIN) return dp[in][si];
+        return dp[in][si] = max(a[si]*b[in] + h(a, b, in+1, si+1, ei), a[ei]*b[in] + h(a, b, in+1, si, ei-1));
+    }
+    int maximumScore(vector<int>& a, vector<int>& mul) {
+        dp = vector<vector<int>>(1005, vector<int>(1005, INT_MIN));
+        return h(a, mul, 0, 0, a.size()-1);
+    }
+};
