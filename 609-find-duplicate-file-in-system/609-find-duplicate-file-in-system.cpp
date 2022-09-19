889ba862d325fa1a1663c400d9b9b42916a33b61
Mandeep Singh Soorma <mandysingh150@gmail.com>
2022-09-19 10:15:40 +0530

    Time: 174 ms (59.54%), Space: 33.9 MB (90.08%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,4 +1,3 @@
-#define ss second
 class Solution {
 public:
     vector<vector<string>> findDuplicate(vector<string>& paths) {
@@ -22,9 +21,9 @@ public:
         }
         
         vector<vector<string>> v;
-        for(auto i: mp) {
-            if(i.ss.size()>=2) {
-                v.push_back(i.ss);
+        for(auto &[a,b]: mp) {
+            if(b.size()>=2) {
+                v.push_back(b);
             }
         }
         return v;
